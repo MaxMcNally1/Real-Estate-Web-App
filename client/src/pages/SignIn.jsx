c0d8205981815444d9e20b1a6d49e5a2e9f1de3a
MaxMcNally <max811811@gmail.com>
2024-07-04 00:49:47 -0400

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice"
 
 export default function SignIn() {
   const [formData, setFormData] = useState({})
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({...formData,
        [e.target.id]: e.target.value,
@@ -14,28 +16,24 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault(); // ---------------- moved from below try
     try {
-      
-    setLoading(true);
-    const res = await fetch('api/auth/signin', 
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      })
-      const data = await res.json()
-      if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
-        return;
-      }
-      setLoading(false);
-      setError(null);
-      navigate('/');
+      dispatch(signInStart());  
+      const res = await fetch('api/auth/signin', 
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
+        })
+        const data = await res.json()
+        if (data.success === false) {
+          dispatch(signInFailure(data.message));
+          return;
+        }
+        dispatch(signInSuccess(data));
+        navigate('/');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);  // ---- changed from data.message 
+      dispatch(signInFailure(error.message)); 
     }
   } 
   return (
