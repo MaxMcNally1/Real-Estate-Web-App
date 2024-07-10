47e2552197c2a36f15399a8198fb8d73dc9160a8
MaxMcNally <max811811@gmail.com>
2024-07-10 01:51:09 -0400

    Create page listing ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,8 +3,10 @@ import { useSelector } from 'react-redux'
 import { useRef, useState, useEffect} from 'react'
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, deleteUserStart, deleteUserSuccess, signOutUserStart} from '../redux/user/userSlice';
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserFailure, 
+  deleteUserStart, deleteUserSuccess, signOutUserStart} from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -82,7 +84,7 @@ export default function Profile() {
 
   const handleDeleteUser = async () => {
     try {
-      dispatch(updateUserStart());
+      dispatch(deleteUserStart());
       const res = await fetch(`/api/user/delete/${currentUser._id}`, {
         method: 'DELETE',
       });
@@ -164,7 +166,12 @@ export default function Profile() {
           onChange={handleChange}
         />
         <button disabled={loading}  className='bg-slate-700 text-white p-3 rounded-lg 
-        uppercase hover:opacity-95 disabled:opacity-95'>{loading ? 'Loading...' : 'Update'}</button>
+        uppercase hover:opacity-95 disabled:opacity-95'>{loading ? 'Loading...' : 'Update'}
+        </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg
+         uppercase text-center hover:opacity-95' to={"/create-listing"}>
+
+        </Link>
       </form>
       <div className='flex justify-between mt-5'>
       <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
