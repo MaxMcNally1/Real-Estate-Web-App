687f25ad7f1cf6150f9f8091e45f89ea3ae9c6d9
MaxMcNally <max811811@gmail.com>
2024-07-21 21:40:01 -0400

    Search page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,18 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
+import Search from "./pages/Search";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
-import UpdateListing from "./pages/UpdateListing.jsx"
+import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
 
 
 
 export default function App() {
   return (
-    // <><h1>HI</h1></>
-
     <BrowserRouter> 
     <Header />
       <Routes> 
@@ -23,7 +22,9 @@ export default function App() {
         <Route path ="/sign-in" element={<SignIn />} />
         <Route path ="/sign-up" element={<SignUp />} />
         <Route path ="/about" element={<About />} />
-        <Route path='/listings/:listingId' element={<Listing />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/listing/:listingId' element={<Listing />} />
+        
         <Route element={<PrivateRoute />}>
         <Route path ="/profile" element={<Profile />} />
         <Route path ="/create-listing" element={<CreateListing />} />
