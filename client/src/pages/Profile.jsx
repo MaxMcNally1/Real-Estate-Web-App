a8e6f895219415d392d8cc56e554548cf2f1204a
MaxMcNally <max811811@gmail.com>
2025-01-25 16:38:35 -0500

    fixed create listing button layout issue

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -205,7 +205,7 @@ export default function Profile() {
         </button>
         <Link className='bg-green-700 text-white p-3 rounded-lg
          uppercase text-center hover:opacity-95' to={"/create-listing"}>
-
+          Create Listing
         </Link>
       </form>
       <div className='flex justify-between mt-5'>
